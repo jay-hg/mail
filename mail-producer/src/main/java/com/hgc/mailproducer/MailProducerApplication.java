b08faa5efebf6137bb65313817c8d7c59a05b2d3
caihg <1175867101@qq.com>
2019-05-07 20:31:18 +0800

    引入mybatis plus generator

--- mail-producer/src/main/java/com/hgc/mailproducer/MailProducerApplication.java
+++ mail-producer/src/main/java/com/hgc/mailproducer/MailProducerApplication.java
@@ -1,11 +1,13 @@
 package com.hgc.mailproducer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = "com.hgc")
 @EnableWebMvc
+@MapperScan("com.hgc.common.mapper")
 public class MailProducerApplication {
 
     public static void main(String[] args) {

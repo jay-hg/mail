3de1f06d6b7822c9c572608dfe8f9684e1620af3
caihg <1175867101@qq.com>
2019-05-13 14:36:18 +0800

    消费者

--- mail-consumer/src/main/java/com/hgc/mailconsumer/mq/Consumer.java
+++ mail-consumer/src/main/java/com/hgc/mailconsumer/mq/Consumer.java
@@ -1,6 +1,8 @@
 package com.hgc.mailconsumer.mq;
 
 import com.hgc.common.entity.MailSend;
+import com.hgc.common.enumeration.MailStatus;
+import com.hgc.common.service.IMailSendService;
 import com.hgc.common.util.FastJsonConvertUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
@@ -9,6 +11,7 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +29,9 @@ public class Consumer {
 
     @Value("${apache.rocketmq.namesrvAddr}")
     private String nameSrv;
+
+    @Autowired
+    IMailSendService mailSendService;
 
     @PostConstruct
     public void init() throws InterruptedException, MQClientException {
@@ -55,6 +61,8 @@ public class Consumer {
                     }
                     MailSend mailSend = FastJsonConvertUtil.convertJSONToObject(mailJson, MailSend.class);
                     log.info("即将消费，mailSend:{}",mailSend);
+                    mailSend.setSendStatus(MailStatus.NEED_OK.getCode());
+                    mailSendService.updateById(mailSend);
                 }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
